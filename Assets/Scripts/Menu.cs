ba5bdcfbcef311a4a2097d846dd2bdf5fd1f8693
itaml <yarik.popov1985@gmail.com>
2021-05-04 12:14:58 +0900

    Final Version

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject authors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && authors.activeSelf)
+        {
+            authors.SetActive(false);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Author()
+    {
+        authors.SetActive(true);
+    }
+
+}
